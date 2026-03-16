4561e5a3a09db4d120ba3ff7b73bd712db447431
myeongseob91 <myeongseob91@naver.com>
2026-03-16 21:45:12 +0900

    3

--- src/components/imageModal.js
+++ src/components/imageModal.js
@@ -1,32 +1,42 @@
 import { ReactComponent as LeftArrow } from '../assets/arrow-left.svg';
 import { ReactComponent as RightArrow } from '../assets/arrow-right.svg';
 
-const imageModal = ({clickedImg, handleRotationRight, handleRotationLeft, setClickedImg}) => {
-
-    const handleClick = (e) => {
-        if (e.target.classList.contains("dismiss")) {
-            setClickedImg(null);
-        }
+const imageModal = ({
+  clickedImg,
+  handleRotationRight,
+  handleRotationLeft,
+  setClickedImg,
+}) => {
+  const handleClick = (e) => {
+    if (e.target.classList.contains('dismiss')) {
+      setClickedImg(null);
     }
+  };
 
-    return <>
-    <div className="overlay dismiss" onClick={handleClick}>
-        <img className="" src={clickedImg} alt="original size"/>
-        <span className="dismiss" onClick={handleClick}>X</span>
+  return (
+    <>
+      <div className='overlay dismiss' onClick={handleClick}>
+        <img className='' src={clickedImg} alt='original size' />
+        <span className='dismiss' onClick={handleClick}>
+          x
+        </span>
         <div onClick={handleRotationRight}>
-            <div>
-                <RightArrow className="overlay-arrows_right"
-                width="50" height="50"/>
-            </div>
+          <div>
+            <RightArrow
+              className='overlay-arrows_right'
+              width='50'
+              height='50'
+            />
+          </div>
         </div>
         <div onClick={handleRotationLeft}>
-            <div>
-                <LeftArrow className="overlay-arrows_left"
-                width="50" height="50"/>
-            </div>
+          <div>
+            <LeftArrow className='overlay-arrows_left' width='50' height='50' />
+          </div>
         </div>
-    </div>
-    </>;
+      </div>
+    </>
+  );
 };
 
 export default imageModal;
